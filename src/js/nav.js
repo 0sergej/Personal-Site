a1fdddebd001918deccfd137f5ed4a6249ff79eb
Sergej Stojanovic <sergej2stojanovic@gmail.com>
2022-04-10 21:18:51 +0200

    Nav JavaScript
    Added script for nav switch.

---
+++ src/js/nav.js
@@ -0,0 +1,11 @@
+const NAV__SWITCH = document.querySelector('.nav__switch--background')
+const NAV__SWITCH__FOREGROUND = document.querySelector(
+    '.nav__switch--foreground'
+)
+const NAV__PRIMARY = document.querySelector('.nav--primary')
+
+NAV__SWITCH.addEventListener('click', () => {
+    NAV__PRIMARY.classList.toggle('block')
+    NAV__SWITCH__FOREGROUND.classList.toggle('switch')
+    NAV__SWITCH.classList.toggle('switch')
+})
