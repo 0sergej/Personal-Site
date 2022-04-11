e7c678c099be7ad1d25167242d1e213fdf8c2cbe
Sergej Stojanovic <sergej2stojanovic@gmail.com>
2022-04-11 22:13:42 +0200

    Nav on Small Screens
    Fix an issue where nav would still be visible on small screens.

--- src/js/nav.js
+++ src/js/nav.js
@@ -4,8 +4,23 @@ const NAV__SWITCH__FOREGROUND = document.querySelector(
 )
 const NAV__PRIMARY = document.querySelector('.nav--primary')
 
-NAV__SWITCH.addEventListener('click', () => {
+let width
+let navOpen = false
+function triggerNav() {
     NAV__PRIMARY.classList.toggle('block')
     NAV__SWITCH__FOREGROUND.classList.toggle('switch')
     NAV__SWITCH.classList.toggle('switch')
+    navOpen = !navOpen
+}
+
+window.onresize = window.onload = function () {
+    width = this.innerWidth
+
+    if (navOpen && width < 752) {
+        triggerNav()
+    }
+}
+
+NAV__SWITCH.addEventListener('click', () => {
+    triggerNav()
 })
